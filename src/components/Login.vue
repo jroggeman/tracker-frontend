3a2c5227cb606924418f94f9c35a64173ea03748
Joel Roggeman <joelroggeman@gmail.com>
2025-02-09 18:10:47 -0600

    Introduce login route, initial example

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>Login</h2>
+    <form>
+        <label for="email">Email</label>
+        <input type="text" name="email" placeholder="john.smith@email.fake">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" />
+
+        <input type="submit" value="Log In" />
+    </form>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Login from '../components/Login.vue'
+</script>
+
+<template>
+  <div class="login">
+    <Login />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .login {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
