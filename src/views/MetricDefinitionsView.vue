2e1473671bb55d9fb374feea07c0b913d44a6ea6
Joel Roggeman <joel@roggeman.me>
2025-02-16 20:52:09 -0800

    Updates

--- src/views/MetricDefinitionsView.vue
+++ src/views/MetricDefinitionsView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useTokenStore } from '../stores/token.store.js'
+import * as bootstrap from 'bootstrap'
 
 const store = useTokenStore();
 
@@ -8,6 +9,8 @@ const metricDefs = ref([]);
 const errMsg = ref('');
 
 const newMetricName = ref('');
+
+const modalWindow = ref()
 
 function addMetric() {
   // TODO validations
@@ -34,6 +37,9 @@ function addMetric() {
       metricDefs.value.push({
         name: data.name
       });
+
+      closeModal();
+      // register below as followup to close event
 
       newMetricName.value = '';
     })
@@ -64,7 +70,18 @@ function fetchData() {
         });
 }
 
-onMounted(fetchData);
+function closeModal() {
+  modalWindow.value.hide();
+}
+
+function showModal() {
+  modalWindow.value.show();
+}
+
+onMounted(() => {
+  fetchData();
+  modalWindow.value = new bootstrap.Modal("#new-metric-modal", {});
+});
 
 </script>
 
@@ -75,30 +92,40 @@ onMounted(fetchData);
     <div>
       <p>Error is {{ errMsg }}.</p>
     </div>
-    <h3>New Metric</h3>
-    <div class="container">
-      <form>
-        <div class="mb-3">
-          <label for="name" class="form-label">Metric Name</label>
-          <input v-model="newMetricName" type="text" class="form-control" name="name" placeholder="Blood Pressure" />
+
+    <div class="row">
+      <div class="col-3">
+        <button type="button" class="btn btn-primary mb-2" @click="showModal" >Add Metric</button>
+        <div class="list-group">
+          <a v-for="metric in metricDefs" href="#" class="list-group-item list-group-item-action">
+            {{ metric.name }}
+          </a>
         </div>
-        <div class="mb-3">
-          <button @click="addMetric" type="button" class="btn btn-primary">Add</button>
-        </div>
-      </form>
-    </div>
-    <h3>List</h3>
-    <div>
-      <!-- TODO bootstrap icons -->
-      <div>
-        <!-- TODO alight to right, use icons? -->
-        <button type="button" class="btn btn-primary m-2">Reload</button>
       </div>
-      <ul class="list-group">
-        <li class="list-group-item" v-for="metric in metricDefs">
-          {{ metric.name }}
-        </li>
-      </ul>
+      <div class="col-9">
+      </div>
+    </div>
+
+    <div class="modal fade" id="new-metric-modal" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title">New Metric</h1>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="mb-3">
+                <label for="name" class="form-label">Metric Name</label>
+                <input v-model="newMetricName" type="text" class="form-control" name="name" placeholder="Blood Pressure" />
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" @click="closeModal">Close</button>
+            <button type="button" class="btn btn-primary" @click="addMetric">Create</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
---
+++ todo.md
@@ -0,0 +1,11 @@
+# TODO
+- [ ] Nested router for metric def editing
+- [ ] Composable for the fetch()s, or use library
+- [ ] persist and secure token state
+- [ ] Clean error displays
+- [ ] Data validation
+- [ ] Login redirects, top right avatar bar
+- [ ] Import parts of bootstrap js that are needed
+- [ ] Cleaner way of constructing API urls - API client?
+- [ ] More composables refactoring with slots and events and such
+- [ ] Consider TanStack Query & composables vs Pinia for state management
